b6badcbdd2ca7e0154837a909df49d5f4d656910
Paulo Vitor <Vitormelo.caridade@outlook.com>
2025-03-29 20:40:52 -0300

    lista encadeada criada

---
+++ listaEncadeada/ListaEncadeada.java
@@ -0,0 +1,4 @@
+package listaEncadeada;
+
+public class ListaEncadeada {
+}
---
+++ listaEncadeada/Main.java
@@ -0,0 +1,11 @@
+package listaEncadeada;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
