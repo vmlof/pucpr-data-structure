cf9d9b0a3b5d2892f201df32cb0e956094151292
Paulo Vitor <Vitormelo.caridade@outlook.com>
2025-04-23 11:27:07 -0300

    lista encadeada circular completa

--- listaEncadeadaCircular/ListaEncadeadaCircular.java
+++ listaEncadeadaCircular/ListaEncadeadaCircular.java
@@ -1,5 +1,77 @@
 package listaEncadeadaCircular;
 
 public class ListaEncadeadaCircular {
+    private No inicio = null;
+
+    private boolean vazia() {
+        return inicio == null;
+    }
+
+    public void inserir(int elemento) {
+        No novoNo = new No(elemento);
+
+        if(inicio == null) {
+            inicio = novoNo;
+            novoNo.setProximo(inicio);
+        }
+        else {
+            No atual = inicio;
+            while(atual.getProximo() != inicio) {
+                atual = atual.getProximo();
+            }
+            atual.setProximo(novoNo);
+            novoNo.setProximo(inicio);
+        }
+    }
+
+    public void remover(int elemento) {
+        if (vazia()) {
+            System.out.println("Erro: lista vazia");
+            return;
+        }
+        No atual = inicio;
+        No anterior = null;
+
+        if (inicio.getElemento() == elemento) {
+
+            if (inicio.getProximo() == inicio) {
+                inicio = null;
+            } else {
+                No ultimo = inicio;
+                while (ultimo.getProximo() != inicio) {
+                    ultimo = ultimo.getProximo();
+                }
+                inicio = inicio.getProximo();
+                ultimo.setProximo(inicio);
+            }
+            return;
+        }
+
+        anterior = inicio;
+        atual = inicio.getProximo();
+        while (atual != inicio) {
+            if (atual.getElemento() == elemento) {
+                anterior.setProximo(atual.getProximo());
+                return;
+            }
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+
+        System.out.println("Elemento " + elemento + " não encontrado.");
+    }
+
+    public void imprimir() {
+        if(vazia()) {
+            System.out.println("Erro: lista vazia");
+            return;
+        }
+        No atual = inicio;
+        while(atual.getProximo() != inicio) {
+            System.out.print(atual.getElemento() + " ");
+            atual = atual.getProximo();
+        }
+        System.out.print(atual.getElemento() + " ");
+    }
 
 }
--- listaEncadeadaCircular/Main.java
+++ listaEncadeadaCircular/Main.java
@@ -1,4 +1,38 @@
 package listaEncadeadaCircular;
 
 public class Main {
+
+    public static void main(String[] args) {
+        ListaEncadeadaCircular lista = new ListaEncadeadaCircular();
+
+        lista.inserir(10);
+        lista.inserir(20);
+        lista.inserir(30);
+        lista.inserir(40);
+        lista.inserir(50);
+        lista.imprimir();
+
+        lista.remover(10);
+        System.out.println();
+        lista.imprimir();
+
+        lista.remover(50);
+        System.out.println();
+        lista.imprimir();
+
+        lista.remover(30);
+        System.out.println();
+        lista.imprimir();
+
+        lista.remover(40);
+        System.out.println();
+        lista.imprimir();
+
+        lista.remover(20);
+        System.out.println();
+        lista.imprimir();
+
+
+    }
+
 }
